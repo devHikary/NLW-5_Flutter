9a2382d03b90219e4940ef6e0fde1fde22b3f9e7
devHikary <wrkkarina@gmail.com>
2021-04-21 15:19:22 -0300

    NLW#5 Aula 1

---
+++ devquiz/lib/splash/splash_page.dart
@@ -0,0 +1,16 @@
+import '../core/app_images.dart';
+import 'package:flutter/material.dart';
+import '../core/app_gradients.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+            gradient: AppGradients.linear,
+          ),
+          child: Center(child: Image.asset(AppImages.logo))),
+    );
+  }
+}
